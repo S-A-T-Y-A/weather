8fde1421ec731440c972f5e9920f48f523163494
satya nandan <satyanandan846@gmail.com>
2021-10-06 05:45:02 +0530

    added the new element search for weather text

--- whether.js
+++ whether.js
@@ -14,7 +14,7 @@ const search=document.querySelector(".search_btn");
 const search_box=document.querySelector(".search_box");
 
 
-getweather("guntur")
+// getweather("guntur")
 const name_ele=document.querySelector(".name");
 const icon_ele=document.querySelector(".icon");
 const body_img=document.querySelector("body");
@@ -22,7 +22,9 @@ const description_ele=document.querySelector(".description");
 const temperature=document.querySelector(".temp");
 const humidity_val=document.querySelector(".humidity");
 const wind=document.querySelector(".wind");
+const text_to_hide=document.querySelector(".text_to_hide")
 function getdata(data){
+  text_to_hide.style.display="none";
 	const {name}=data;
     
     const {icon,description}=data.weather[0];
@@ -34,10 +36,10 @@ function getdata(data){
     console.log(name,icon,description,temp,humidity,icon,speed*10);
     name_ele.innerHTML=name;
     icon_ele.src =
-    "https://openweathermap.org/img/wn/" + icon + ".png";
+    "https://openweathermap.org/img/wn/" + icon + "@2x.png";
     description_ele.innerHTML=description;
     temperature.innerHTML="Temperature : "+temp+" °c ";
-    wind.innerHTML="Wind Speed : "+speed*10+" km/hr";
+    wind.innerHTML="Wind Speed : "+speed+" km/hr";
     humidity_val.innerHTML="Humidity : "+humidity+" g.m<sup>-3</sup>";
     body_img.style.background="url(https://source.unsplash.com/1600x1900/?"+name+")";
 }
